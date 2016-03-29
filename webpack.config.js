cfa59c0d821fbaa9f00f5c47a9083caed58fcfe7
Airead Fan <fgh1987168@gmail.com>
2016-03-29 14:52:26 +0800

    feat:config start kit

---
+++ webpack.config.js
@@ -0,0 +1,69 @@
+const path = require('path');
+const merge = require('webpack-merge');
+const webpack = require('webpack');
+
+const TARGET = process.env.npm_lifecycle_event;
+process.env.BABEL_ENV = TARGET;
+
+const PATHS = {
+    app: path.join(__dirname, 'app'),
+    build: path.join(__dirname, 'public')
+};
+
+const common = {
+    entry: {
+        app: PATHS.app
+    },
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    },
+    output: {
+        path: PATHS.build,
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [{
+            test: /\.jsx?$/,
+            loaders: ['babel?cacheDirectory'],
+            include: PATHS.app
+        }, {
+            test: /\.css$/,
+            loaders: [
+                'style?sourceMap',
+                'css?modules&importLoaders=1&localIdentName=[path]___[name]__[local]___[hash:base64:5]'
+            ]
+        }]
+    }
+};
+
+if (TARGET === 'start' || !TARGET) {
+    module.exports = merge(common, {
+        devtool: 'source-map',
+        devServer: {
+            contentBase: './public',
+            historyApiFallback: true,
+            hot: true,
+            inline: true,
+            progress: true,
+
+            stats: 'errors-only',
+            host: process.env.HOST || '0.0.0.0',
+            port: process.env.PORT
+        },
+        plugins: [
+            new webpack.HotModuleReplacementPlugin()
+        ]
+    });
+}
+
+if (TARGET === 'build') {
+    module.exports = merge(common, {
+        plugins: [
+            new webpack.optimize.UglifyJsPlugin({
+                compress: {
+                    warnings: false
+                }
+            })
+        ]
+    });
+}
